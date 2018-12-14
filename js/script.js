0b355492f83cf7543790043ab276c14bb71140c2
Bryan Vo <derpynoobking@gmail.com>
2018-12-14 02:26:22 +0000

    Your message here

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* global $ */
 
-$(document).ready(function() {
+
     $("button").click(function() {
         var name = $("#name").val();
         var techPlacement = "TBD";
@@ -8,7 +8,36 @@ $(document).ready(function() {
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
-        var totalScore;
+        var totalScore = 0;
+        if (q1Result === "Yes") {
+            totalScore +4;
+        }
+        else if (q1Result === "Mixed"){ 
+            totalScore + 1;
+        }
+        else if (q1Result === "No") { 
+            totalScore + 1;
+        }
+        else if (q2Result === "Front"){ 
+            totalScore + 1;
+        }
+        else if (q2Result === "Behind"){ 
+            totalScore + 3;
+        }
+        else if (q2Result === "Mixed"){ 
+            totalScore + 2;
+        }
+        else if (q3Result === "Support"){
+            totalScore +2;
+        }
+        else if (q3Result === "Squishy"){ 
+            totalScore +1;
+        }
+        else if (q2Result === "Mixed"){ 
+            totalScore + 3;
+        }
+    if (totalScore >= 8) {
+        alert("Attack Damage Carry is good for you " + name +"!");
+    }
     });
 
-});
